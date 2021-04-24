5d6546ea7b638eeb5da7f7aad261044db04d827a
Timothy Foster <tf.auroratide@gmail.com>
2021-04-24 14:03:13 -0500

    Remove unused MyApp component

--- examples/src/MyApp.svelte
+++
@@ -1,9 +0,0 @@
-<svelte:options tag="my-app" />
-
-<h1>Hello world!</h1>
-
-<style>
-    h1 {
-        color: red;
-    }
-</style>
--- examples/src/main.ts
+++ examples/src/main.ts
@@ -1,4 +1,3 @@
-import './MyApp.svelte'
 import './BlurExample.svelte'
 import './FadeExample.svelte'
 import './CustomExample.svelte'

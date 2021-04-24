812a45f012ecb8f6e792e7a14a285fa15737b411
Timothy Foster <tf.auroratide@gmail.com>
2021-04-24 09:57:09 -0500

    Use new function for svelte built-in transitions

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,8 @@
-import type { TransitionConfig, FadeParams, BlurParams } from 'svelte/transition'
-import { linear, cubicInOut } from 'svelte/easing'
+import type { TransitionConfig } from 'svelte/transition'
+import {
+    fade as sFade,
+    blur as sBlur,
+ } from 'svelte/transition'
 
 /**
  * @method: Convert a svelte transition into a form that works for custom elements
@@ -18,57 +21,5 @@ export function forCustomElement<T>(original: (node: HTMLElement, params: T) => 
     }
 }
 
-/**
- * @method Svelte fade transition
- * @param node node
- * @param param1 options
- * @returns transition config
- */
-export const fade = (node: HTMLElement, {
-    delay = 0,
-    duration = 400,
-    easing = linear,
-}: FadeParams = {}): TransitionConfig => {
-    const o = +getComputedStyle(node).opacity
-
-    return {
-        delay,
-        duration,
-        easing,
-        css: t => `opacity: ${t * o}`,
-        tick: t => {
-            node.style.opacity = `${t * o}`
-        },
-    }
-}
-
-/**
- * @method Svelte blur transition
- * @param node node
- * @param param1 options
- * @returns transition config
- */
-export const blur = (node: HTMLElement, {
-    delay = 0,
-    duration = 400,
-    easing = cubicInOut,
-    amount = 5,
-    opacity = 0,
-}: BlurParams = {}): TransitionConfig => {
-    const style = getComputedStyle(node)
-    const target_opacity = +style.opacity
-    const f = style.filter === 'none' ? '' : style.filter
-
-    const od = target_opacity * (1 - opacity)
-
-    return {
-        delay,
-        duration,
-        easing,
-        css: (_t, u) => `opacity: ${target_opacity - (od * u)}; filter: ${f} blur(${u * amount}px);`,
-        tick: (_t, u) => {
-            node.style.opacity = `${target_opacity - (od * u)}`
-            node.style.filter = `${f} blur(${u * amount}px)`
-        },
-    }
-}
+export const fade = forCustomElement(sFade)
+export const blur = forCustomElement(sBlur)

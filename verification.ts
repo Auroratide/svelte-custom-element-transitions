537436105813c47fa06bb67cb8f40179841a4239
Timothy Foster <tf.auroratide@gmail.com>
2021-04-23 20:58:56 -0500

    Refactor verification, extracting common testing pattern

--- verification.ts
+++ verification.ts
@@ -1,53 +1,65 @@
 import { Selector } from 'testcafe'
 
-fixture `A Test`
+fixture `Verify @auroratide/svelte-custom-element-transitions`
     .page `http://localhost:5000`
 
-test('testing', async t => {
+const POLL_INTERVAL = 50 // milliseconds
+
+type StyleList = { [prop: string]: string }
+
+class TransitioningComponent {
+    private name: string
+    private component: Selector
+    private isTransitioning: (style: StyleList) => boolean
+
+    constructor(name: string, isTransitioning: (style: StyleList) => boolean) {
+        this.name = name
+        this.component = Selector(name).shadowRoot()
+        this.isTransitioning = isTransitioning
+    }
+
+    private get title(): Selector {
+        return this.component.find('h2')
+    }
+
+    private get trigger(): Selector {
+        return this.component.find('button')
+    }
+
+    private async style(): Promise<StyleList> {
+        return await this.title.style
+    }
+    
+    async verify(t: TestController): Promise<void> {
+        await t.expect(this.title.exists).ok()
+        await t.click(this.trigger)
+
+        const transitioning: boolean[] = []
+        while (await this.title.exists) {
+            transitioning.push(this.isTransitioning(await this.style()))
+            await t.wait(POLL_INTERVAL)
+        }
+
+        await t.expect(transitioning.some(v => v)).ok(`The transition for ${this.name} did not occur`)
+    }
+}
+
+test('Transitions', async t => {
     const title = Selector('header h1')
     await t.expect(title.exists).ok()
 
-    const fade = Selector('fade-example').shadowRoot()
+    await new TransitioningComponent('fade-example', (style: StyleList) => {
+        const opacity = parseFloat(style['opacity'])
+        return 0 < opacity && opacity < 1
+    }).verify(t)
 
-    await t.click(fade.find('button'))
-    await t.expect(fade.find('h2').exists).ok()
-
-    const transitioning: number[] = []
-    while (await fade.find('h2').exists) {
-        const opacity = parseFloat((await fade.find('h2').style)['opacity'])
-        
-        if (0 < opacity && opacity < 1)
-            transitioning.push(opacity)
-        
-        await t.wait(50)
-    }
-
-    await t.expect(transitioning.length).gt(0, 'The transition did not occur')
-
-    /****************************/
-
-    const blur = Selector('blur-example').shadowRoot()
-
-    await t.click(blur.find('button'))
-    await t.expect(blur.find('h2').exists).ok()
-
-    const transitioningBlur: number[] = []
-    while (await blur.find('h2').exists) {
-        const style = await blur.find('h2').style
+    await new TransitioningComponent('blur-example', (style: StyleList) => {
         const blurMatch = style['filter'].match(/blur\((.*?)\)/)
         let blurFactor = 0
         if (blurMatch) {
             blurFactor = parseFloat(blurMatch[1])
         }
-        // const blurFactor = parseFloat(style['filter'].match(/blur\((.*?)\)/)[1])
-        
-        if (0 < blurFactor && blurFactor < 5)
-            transitioningBlur.push(blurFactor)
-        
-        await t.wait(50)
-    }
 
-    console.log(transitioningBlur)
-
-    await t.expect(transitioningBlur.length).gt(0, 'The transition did not occur')
+        return 0 < blurFactor && blurFactor < 5
+    }).verify(t)
 })
